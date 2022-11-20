5022269be055329c7631049267ce837dce72c703
Ryan Builds <ryan.gabrinao@gmail.com>
2022-11-20 05:44:04 -0800

    implemented ISG for dynamic routing

---
+++ pages/case/[slug].js
@@ -0,0 +1,69 @@
+import React from "react";
+import { sanityClient } from "../../sanity";
+
+function Project({ project }) {
+  return <div>Project: {project.title}</div>;
+}
+
+export default Project;
+
+export const getStaticPaths = async () => {
+  const query = `*[_type == "projects"]{
+    _id,
+    slug {
+      current
+    },
+  }`;
+
+  const projects = await sanityClient.fetch(query);
+
+  const paths = projects.map((project) => ({
+    params: {
+      slug: project.slug.current,
+    },
+  }));
+
+  return {
+    paths,
+    fallback: "blocking",
+  };
+};
+
+export const getStaticProps = async ({ params }) => {
+  const query = `*[_type == "projects" && slug.current == $slug][0]{
+    _id,
+    title,
+    slug,
+    href,
+    mainImage,
+    desktopImage,
+    mobileImage,
+    galleryFullWidthMobile,
+    excerpt,
+    tags
+  }`;
+
+  const query2 = `*[_type == "projects" && slug.current != $slug]`;
+
+  const project = await sanityClient.fetch(query, {
+    slug: params?.slug,
+  });
+
+  const otherProjects = await sanityClient.fetch(query2, {
+    slug: params?.slug,
+  });
+
+  if (!project) {
+    return {
+      notFound: true,
+    };
+  }
+
+  return {
+    props: {
+      project,
+      otherProjects,
+    },
+    revalidate: 60,
+  };
+};
--- src/components/Works.js
+++ src/components/Works.js
@@ -1,9 +1,23 @@
 import React from "react";
+import Link from "next/link";
+import { urlFor } from "../../sanity";
 
 function Works({ projects }) {
   return (
     <section>
       <h2>Come check out my projects</h2>
+      {projects.map((project) => {
+        return (
+          <div key={project._id}>
+            <h1>{project.title}</h1>
+            <div className="overflow-hidden rounded-xl w-[80%] mx-auto">
+              <Link href={`/case/${project.slug.current}`}>
+                <img src={urlFor(project.mainImage).url()} alt="" />
+              </Link>
+            </div>
+          </div>
+        );
+      })}
     </section>
   );
 }
