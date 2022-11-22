6a48587d0b177104217b2e43375de5811be8d097
Ryan Builds <ryan.gabrinao@gmail.com>
2022-11-22 02:07:13 -0800

    redesigned hero section

--- sanity/schemas/schema.js
+++ sanity/schemas/schema.js
@@ -7,6 +7,7 @@ import schemaTypes from "all:part:@sanity/base/schema-type";
 // We import object and document schemas
 import projects from "./projects";
 import siteSettings from "./siteSettings";
+import songs from "./songs";
 
 // Then we give our schema to the builder and provide the result to Sanity
 export default createSchema({
@@ -21,5 +22,6 @@ export default createSchema({
     // When added to this list, object types can be used as
     // { type: 'typename' } in other document schemas
     siteSettings,
+    songs,
   ]),
 });
