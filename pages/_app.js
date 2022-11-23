33ed0e9132f508a1c7af4994bef0ca3681096b2b
Ryan Builds <ryan.gabrinao@gmail.com>
2022-11-22 18:33:18 -0800

    adjusted lenis easing

--- pages/_app.js
+++ pages/_app.js
@@ -40,7 +40,7 @@ function MyApp({ Component, pageProps }) {
   const route = useRouter();
   useEffect(() => {
     const lenis = new Lenis({
-      duration: 1.7,
+      duration: 1.2,
       easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)), // https://www.desmos.com/calculator/brs54l4xou
       direction: "vertical", // vertical, horizontal
       gestureDirection: "vertical", // vertical, horizontal, both
