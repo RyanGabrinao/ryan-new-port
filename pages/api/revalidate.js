9626f0912043f6e3caee90d90a1d71aea20fb180
Ryan Builds <ryan.gabrinao@gmail.com>
2022-12-01 15:24:07 -0800

    updated revalidate.js

--- pages/api/revalidate.js
+++ pages/api/revalidate.js
@@ -1,27 +1,33 @@
-import { SIGNATURE_HEADER_NAME, isValidSignature } from "@sanity/webhook";
+import { isValidRequest } from "@sanity/webhook";
 
-const handler = async (req, res) => {
-  //authenticating the webhook
-  try {
-    const signature = req.headers[SIGNATURE_HEADER_NAME].toString();
-    if (
-      !isValidSignature(
-        JSON.stringify(req.body),
-        signature,
-        process.env.SANITY_WEBHOOK_SECRET
-      )
-    )
-      return res.status(401).json({ msg: "Invalid request!" });
+const secret = process.env.SANITY_WEBHOOK_SECRET;
 
-    //getting payload
-    const { slug } = req.body;
-    // await res.revalidate(`/case/`);
-    await res.revalidate(`/case/${slug}`);
-
-    res.status(200).json({ msg: "Product pages revalidated." });
-  } catch (error) {
-    res.status(500).json({ err: "Something went Wrong!" });
+export default async function handler(req, res) {
+  if (req.method !== "POST") {
+    console.error("Must be a POST request");
+    return res.status(401).json({ message: "Must be a POST request" });
   }
-};
 
-export default handler;
+  if (!isValidRequest(req, secret)) {
+    res.status(401).json({ message: "Invalid signature" });
+    return;
+  }
+
+  try {
+    const {
+      body: { type, slug },
+    } = req;
+
+    switch (type) {
+      case "post":
+        await res.revalidate(`/news/${slug}`);
+        return res.json({
+          message: `Revalidated "${type}" with slug "${slug}"`,
+        });
+    }
+
+    return res.json({ message: "No managed type" });
+  } catch (err) {
+    return res.status(500).send({ message: "Error revalidating" });
+  }
+}
